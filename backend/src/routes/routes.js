27a746967400d60f0cde273bf868faaa1ed676ce
MURILO REIS DOTA <murilo.dota2@senaisp.edu.br>
2026-09-17 10:25:57 -0300

    update: teste

---
+++ backend/src/routes/routes.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+//import etapasArquivosRoutes from "./etapasArquivoRoutes.js";
+//import advogadosCasosRoutes from "./advogadosCasosRoutes.js";
+
+//import administradorRoutes from "./administradorRoutes.js";
+//import advogadoRoutes from "./advogadoRoutes.js";
+//import authRoutes from "./authRoutes.js";
+
+import casoRoutes from "./casoRoutes.js";
+//import etapasRoutes from "./etapasRoutes.js";
+//import tarefasRoutes from "./tarefasRoutes.js";
+//import arquivosRoutes from "./arquivosRoutes.js";
+
+const router = Router();
+
+
+// router.use('/auth', authRoutes);
+// router.use('/advogado', advogadoRoutes);
+//router.use('/administrador', administradorRoutes);
+//router.use('/etapasArquivos', etapasArquivosRoutes);
+// router.use('/advogadosCasos', advogadosCasosRoutes);
+router.use('/casos', casoRoutes);
+//router.use('/etapas', etapasRoutes);
+//router.use('/tarefas', tarefasRoutes);
+// router.use('/arquivos', arquivosRoutes);
+
+
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import router from './routes/routes.js';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.use(express.json())
+
+app.use('/', router);
+
+
+app.listen(process.env.SERVER_PORT || 8080,() =>{
+    console.log(`Servidor rodando em https:localhost:${process.env.SERVER_PORT}`)
+})
