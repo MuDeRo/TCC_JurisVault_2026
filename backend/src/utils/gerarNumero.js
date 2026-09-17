2e6d40ec8df43686a431925ccb5ebad309f345c7
MURILO REIS DOTA <murilo.dota2@senaisp.edu.br>
2026-09-17 09:29:36 -0300

    feat: adicionar geração de identificador para padrão CNJ

---
+++ backend/src/utils/gerarNumero.js
@@ -0,0 +1,14 @@
+//Função criada para gerar automaticamente um identificador com 20 números aleatórios, que será usado para identificar os casos após sua criação
+function gerarNumerosAleatorios() {
+    const numeros = [];
+
+    for (let i = 0; i < 20; i++) {
+        const numeroAleatorio = Math.floor(Math.random() * 9) + 1; //Sorteia um decimal até 0.9, multiplica por 9, corta a vírgula e soma 1 (Gera de 1 a 9)
+        numeros.push(numeroAleatorio);
+    }
+
+    return numeros;
+}
+
+// Exemplo de uso:
+console.log(gerarNumerosAleatorios());
