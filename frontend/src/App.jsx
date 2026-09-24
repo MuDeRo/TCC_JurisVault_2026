7074394ec9376cd453f8e5a157f6aef9d9d5af2e
KAICK EDUARDO ARAUJO DA SILVA <kaick.silva4@senaisp.edu.br>
2026-09-24 08:34:05 -0300

    update: atualizaçao do conteudo relacionado com as paginas principais ee o fluxo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+// Página Pública de Entrada (Portal com os 3 cartões)
 import CampoPaginaAdm from './pages/PaginaInicial/CampoPaginaAdm';
 import LoginAdvogados from './pages/LoginAdvogados/LoginAdvogados';
 import CadastroAdvogados from './pages/CadastroAdvogados/CadastroAdvogados';
-import Administradores from './pages/Administradores/Administradores';
-import PainelAdministrativo from './pages/PainelAdministrativo/PainelAdministrativo';
 
+// Páginas Internas do Sistema
+import PainelAdministrativo from './pages/PainelAdministrativo/PainelAdministrativo';
+import Administradores from './pages/Administradores/Administradores';
 import Processos from './pages/Processos/Processos';
 import CadastroProcesso from './pages/Processos/CadastroProcesso';
 import Clientes from './pages/Processos/Clientes';
 import Documentos from './pages/Processos/Documentos';
 import Prazos from './pages/Processos/Prazos';
 
+import Layout from './components/layoutPagina/layout';
+
 export default function App() {
   return (
     <Routes>
-      
+      {/* ROTAS PÚBLICAS (Sem barra lateral) */}
       <Route path="/" element={<CampoPaginaAdm />} />
-      <Route path="/inicio" element={<CampoPaginaAdm />} />
-      <Route path="/cadastro" element={<CadastroAdvogados />} />
       <Route path="/login" element={<LoginAdvogados />} />
-      <Route path="/painel-administrativo" element={<PainelAdministrativo />} />
-      <Route path="/administrador" element={<Administradores />} />
-      <Route path="/processos" element={<Processos />} />
-      <Route path="/novo-caso" element={<CadastroProcesso />} />
-      <Route path="/clientes" element={<Clientes />} />
-      <Route path="/documentos" element={<Documentos />} />
-      <Route path="/prazos" element={<Prazos />} />
+      <Route path="/cadastro" element={<CadastroAdvogados />} />
+
+      {/* ROTAS INTERNAS DO SISTEMA (Com barra lateral) */}
+      <Route element={<Layout />}>
+        <Route path="/inicio" element={<PainelAdministrativo />} />
+        <Route path="/painel-administrativo" element={<PainelAdministrativo />} />
+        <Route path="/administrador" element={<Administradores />} />
+        <Route path="/processos" element={<Processos />} />
+        <Route path="/novo-caso" element={<CadastroProcesso />} />
+        <Route path="/clientes" element={<Clientes />} />
+        <Route path="/documentos" element={<Documentos />} />
+        <Route path="/prazos" element={<Prazos />} />
+      </Route>
 
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
